c7ab664c8f301a2a9fb0f523aea6edd0e576f1ed
Harsh Modi <modi.harsh04@gmail.com>
2020-12-24 23:17:25 -0600

    update more details

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,26 @@
 import React from "react";
 import {UniqueKey} from "./util";
+import {tagline} from "../assets/ascii_art/tagline";
 
 export const About = () => (
   <React.Fragment key={UniqueKey()}>
-    Hello, I am a software engineer with Master's in Computer Science. I am currently
-    living in Illinois, USA. I come from Ahmadabad, Gujarat, India. I love to solve 
-    problems, challenging tasks and that is the reason I chose Computer Science as 
-    my career.
+    Hello, I am a software engineer with a Masters's in Computer Science. I am currently living in Illinois, USA. I come from Ahmadabad, Gujarat, India. I love to solve problems, challenging tasks and that is the reason why I chose Computer Science as my career.
     <br /><br />
-    Outside work I love traveling, being amidst nature, trying new food, doing yoga, 
-    playing and watching sport, experiment with new things/adventures, watching movies,
-    shows, and get involved in intellectual conversations. I love being on the
-    road and explore new places, food or culture. 
+    Outside work I love traveling, being amidst nature, trying new food, doing yoga, playing and watching sport, experiment with new things/adventures, watching movies, shows, and get involved in intellectual conversations. I love being on the road and explore new places, food, or culture. 
     <br /><br />
-    I disguise myself as a life long learner, analytical, positive thinker, 
-    cheerful, and one who loves to spend time with family and friends.
+    I disguise myself as a life long learner, analytical, positive thinker, cheerful, and one who loves to spend time with family and friends.
+    <br /><br />
+    The tagline for me would be:
+    <pre style={style.tagline}>{tagline}</pre>
   </React.Fragment>
 )
+
+const style = {
+    tagline:{
+	lineHeight: '4pt',
+	fontSize: '4pt',
+    	letterSpacing: '-0.8pt',
+    	color: 'var(--color-blue)',
+    	fontWeight: '900',
+    }
+}
